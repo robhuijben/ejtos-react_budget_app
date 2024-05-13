8e6d8c79a75740ddc95070fb22accd64b04813a6
Rob Huijben <mail@robhuijben.com>
2024-05-13 13:51:14 +0700

    feature: Budget allocation initial code

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,28 @@
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
+const Budget = () => {
+  const { budget } = useContext(AppContext);
+  const [newBudget, setNewBudget] = useState(budget);
+  const handleBudgetChange = (e) => {
+    setNewBudget(e.target.value);
+  };
+  return (
+    <div className="alert alert-secondary">
+      <span>Budget: € </span>
+      <input
+        maxLength="10"
+        style={{ width: "100px" }}
+        type="number"
+        step="10"
+        className="me-2"
+        value={newBudget}
+        onChange={handleBudgetChange}
+      ></input>
+      <button className="btn btn-primary" onClick={() => setNewBudget(budget)}>
+        Set budget
+      </button>
+    </div>
+  );
+};
+export default Budget;
