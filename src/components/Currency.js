2afcd62953801e0cb77ab25a2899cdd2d9846ccd
Rob Huijben <mail@robhuijben.com>
2024-05-13 18:28:29 +0700

    refactor: Currency dropdown styling and events

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,35 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { Dropdown, Button, ButtonGroup } from "react-bootstrap";
 import { AppContext } from "../context/AppContext";
 
 const Currency = () => {
   const { dispatch } = useContext(AppContext);
+  const [currencyName, setCurrencyName] = useState("£ Pound");
 
-  const changeCurrency = (val) => {
+  const changeCurrency = (val, name) => {
     dispatch({
       type: "CHG_CURRENCY",
       payload: val,
     });
+
+    setCurrencyName(name);
   };
 
   return (
-    <div className="alert alert-info">
-      <div className="input-group">
-        <div className="input-group-text">Currency</div>
-        {
-          <select
-            className="form-control"
-            name="Location"
-            id="Location"
-            onChange={(event) => changeCurrency(event.target.value)}
-          >
-            <option value="£">£ Pound</option>
-            <option value="$">$ Dollar</option>
-            <option value="€">€ Euro</option>
-            <option value="₹">₹ Ruppee</option>
-          </select>
-        }
-      </div>
-    </div>
+    <>
+      <Dropdown>
+        <Dropdown.Toggle className="w-100 d-flex align-items-center justify-content-between">
+          Currency ({currencyName})
+        </Dropdown.Toggle>
+        <Dropdown.Menu className="w-100">
+          <Dropdown.Item onClick={() => changeCurrency("£", "£ Pound")}>
+            £ Pound
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => changeCurrency("$", "$ Dollar")}>
+            $ Dollar
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => changeCurrency("€", "€ Euro")}>
+            € Euro
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => changeCurrency("₹", "₹ Ruppee")}>
+            ₹ Ruppee
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </>
   );
 };
 
